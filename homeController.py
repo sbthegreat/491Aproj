93f7475952935e69ad4faa34e9b8dee39f70b9f3
sbthegreat <connorbl93@yahoo.com>
2017-05-06 15:51:44 -0700

    updating home page
    correct page mapping, quitting functionality, displays user name

--- homeController.py
+++ homeController.py
@@ -4,6 +4,15 @@ import constants
 
 def Control(state):
     homeScreen.Draw(state)
-    if state.direction == constants.Direction.RIGHT:
+    if state.direction == constants.Direction.UP:
+        state.setPage(constants.Page.HOME)
+        state.resetDirection()
+    elif state.direction == constants.Direction.RIGHT:
         state.setPage(constants.Page.GAME)
+        state.resetDirection()
+    elif state.direction == constants.Direction.LEFT:
+        state.setPage(constants.Page.HABITS)
+        state.resetDirection()
+    elif state.direction == constants.Direction.DOWN:
+        state.active = False
         state.resetDirection()
--- homeScreen.py
+++ homeScreen.py
@@ -2,7 +2,8 @@ import mainController as master
 import numpy as np
 import cv2
 import constants
+
 def Draw(state):
     screen = np.zeros((800,800,3), np.uint8)
-    cv2.putText(screen, "Swipe right for game", (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0))
+    cv2.putText(screen, "Welcome, " + state.currentUser.name, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255))
     cv2.imshow('BrushSmart', screen)
