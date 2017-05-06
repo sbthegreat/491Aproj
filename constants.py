ee23e90ac7fedf624e5e0876d26768bc7867718b
sbthegreat <connorbl93@yahoo.com>
2017-05-06 15:34:49 -0700

    fine-tuning direction tracking-related constants

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ class Page(Enum):
     HISCORES = 3
     HABITS = 4
     ENTERNAME = 5
-    LOGIN = 6
+    CALIBRATE = 6
 
 class Direction(Enum):
     UP = 1
@@ -17,17 +17,20 @@ class Direction(Enum):
 
 TRAIL_COLOR = (255,0,0) # the BGR color of the trail following the tracked object
 FPS = 60 # the FPS the game runs at
-POINT_LIMIT = FPS / 4; # the maximum number of points stored
+POINT_LIMIT = 5; # the maximum number of points stored
 JITTER_POINT = 1 # the pixel difference needed to actually draw a new line segment (I'm not sure if this really works as-is)
-MIN_SWIPE = 50 # the minimum number of pixels the distance a swipe has to move before it is registered
-COOLDOWN = 1 # the number of seconds swipes are not tracked after a swipe occurs
-MIN_SIZE = 10000 # the smallest size a collection of thresholded pixels can be to be registered as a tracked object
+MIN_SWIPE = 150 # the minimum number of pixels the distance a swipe has to move before it is registered
+COOLDOWN = 0.5 # the number of seconds swipes are not tracked after a swipe occurs
+MIN_SIZE = 10000 # the smallest size a collection of thresholded pixels can be to register as a tracked object
 
 # the HSV color values that are tracked
-LOW_H = 0
-HIGH_H = 40
-LOW_S = 61
-HIGH_S = 114
-LOW_V = 103
-HIGH_V = 166
+LOW_H = 54
+HIGH_H = 82
+LOW_S = 77
+HIGH_S = 138
+LOW_V = 42
+HIGH_V = 128
 # screen size should be a constant
+
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ALPHA_SIZE = 26
