ca42c3f786f7022b6f8712c53b2e6a9832b00ddf
sbthegreat <connorbl93@yahoo.com>
2017-05-08 03:45:37 -0700

    adding game fields
    quadrant/germ-related

--- systemState.py
+++ systemState.py
@@ -2,19 +2,27 @@ import constants
 import collections
 import user
 import datetime
+import constants
 
 class SystemState():
     def __init__(self):
         self.currentUser = user.User(None)
         self.trailPoints = collections.deque()
-        self.currentPage = constants.Page.HABITS
-        self.direction = constants.Direction.NONE 
-        self.removedPoint = (0,0)
+        self.currentPage = constants.Page.HOME
+        self.direction = constants.Direction.NONE
         self.lastSwipe = 0
         self.userList = []
         self.alphabetIndex = 0
         self.selectedMonth = datetime.date.today().month
         self.active = True
+        self.gameStart = 0
+        self.lastGermCheck = 0
+        self.germList = []
+        self.gameScore = 0
+        self.quadrantOrder = [constants.Quadrant.TOPRIGHT, constants.Quadrant.TOPLEFT, constants.Quadrant.BOTTOMLEFT, constants.Quadrant.BOTTOMRIGHT]
+        self.curId = 0
+        self.currentQuadrant = 0
+        self.lastQuadrantTransition = 0
         
     def setPage(self, newPage):
         self.currentPage = newPage
@@ -22,10 +30,8 @@ class SystemState():
     def resetDirection(self):
         self.direction = constants.Direction.NONE
 
-    # adds points to the trail, pops off oldest point if past length limit and returns it
+    # adds points to the trail, pops off oldest point if past length limit
     def addPoint(self, x):
         self.trailPoints.append(x)
         if len(self.trailPoints) > constants.POINT_LIMIT:
-            return self.trailPoints.popleft()
-        else:
-            return self.trailPoints[0]
+            self.trailPoints.popleft()
