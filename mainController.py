03ea14f1759705d1451babaf7142f5ea60157f8d
sbthegreat <connorbl93@yahoo.com>
2017-05-08 03:44:37 -0700

    adding new pages

--- mainController.py
+++ mainController.py
@@ -10,19 +10,23 @@ import homeController
 import gameController
 import nameController
 import habitsController
+import calibrateController
+import hiscoreController
+import confirmController
 from enum import Enum
 import constants
 import systemState
 import user
+import startup
 
 def main():
-    directory = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.dirname(os.path.abspath(__file__)) + "\\users"
     state = systemState.SystemState()
-    # state.currentUser, state.userList, newUser = startup.getUser()
-    # if newUser:
-        # state.currentPage = constants.Page.ENTERNAME
-    # else:
-        # state.currentPage = constants.Page.HOME
+    state.currentUser, state.userList, newUser = startup.getUser()
+    if newUser:
+        state.currentPage = constants.Page.ENTERNAME
+    else:
+        state.currentPage = constants.Page.HOME
     
     motionThread = threading.Thread(target=motionTracking, args=(state,))
     logicThread = threading.Thread(target=changeData, args=(state,))
@@ -32,10 +36,10 @@ def main():
     motionThread.join()
     logicThread.join()
     
-    #pickle.dump(state.currentUser, open("users\\" + state.currentUser.name, 'wb'))
+    pickle.dump(state.currentUser, open("users\\" + state.currentUser.name, 'wb'))
 
 def motionTracking(state):
-    threadStart = time.time();
+    time.sleep(2)
     cap = cv2.VideoCapture(0)
     
     currentX = -1
@@ -44,14 +48,13 @@ def motionTracking(state):
     prevY = -1
 
     while state.active:
-        # Capture frame-by-frame
         ret, frame = cap.read()
         frame = cv2.flip(frame,1) # flips along y-axis
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
         thresh = cv2.inRange(hsv, (constants.LOW_H, constants.LOW_S, constants.LOW_V), (constants.HIGH_H, constants.HIGH_S, constants.HIGH_V))
         
-        #morphological opening (removes small objects from the foreground)
+        # removes small objects from foreground
         thresh = cv2.erode(thresh, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
         thresh = cv2.dilate(thresh, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
         
@@ -64,7 +67,7 @@ def motionTracking(state):
             currentX = int(moment10 / trackedArea)
             currentY = int(moment01 / trackedArea)
         
-        state.removedPoint = state.addPoint((currentX, currentY))
+        state.addPoint((currentX, currentY))
         prevX = currentX
         prevY = currentY
 
@@ -74,13 +77,10 @@ def motionTracking(state):
         setDirection(dx, dy, state)
         
         cv2.waitKey(1)
-        # if time.time() - threadStart > 30: # quit with q
-            # break
         time.sleep(1 / constants.FPS)
-    #clean-up capture after completion
+    
     cap.release()
     cv2.destroyAllWindows()
-    print("Tracking thread complete")
 
 def changeData(state):
     threadStart = time.time()
@@ -89,22 +89,19 @@ def changeData(state):
             homeController.Control(state)
         elif state.currentPage == constants.Page.GAME:
             gameController.Control(state)
-        elif state.currentPage == constants.Page.HISCORES:
-            a = 0 # replace all a=0 lines with commented out controller lines
-            #goto hiscoreController.Control(state)
+        elif state.currentPage == constants.Page.HISCORE:
+            hiscoreController.Control(state)
         elif state.currentPage == constants.Page.HABITS:
             habitsController.Control(state)
         elif state.currentPage == constants.Page.ENTERNAME:
             nameController.Control(state)
         elif state.currentPage == constants.Page.CALIBRATE:
-            a = 0
-            #goto calibrateController.Control(state)
+            calibrateController.Control(state)
+        elif state.currentPage == constants.Page.CONFIRM:
+            confirmController.Control(state)
         
         cv2.waitKey(1)
-        # if time.time() - threadStart > 30: # quit with q
-            # break
         time.sleep(1 / constants.FPS)
-    print("Logic thread complete")
 
 def setDirection(dx, dy, state):
     length = math.hypot(dx, dy)
@@ -113,7 +110,7 @@ def setDirection(dx, dy, state):
         if dx != 0:
             slope = dy / dx
         else:
-            slope = 50 # placeholder - any value >1 works
+            slope = 50 # any value >1 works
             
         if dy >= 0 and dx > 0:
             quadrant = 1
