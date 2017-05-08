b578a9cae836bb75314fdb39609cece2d313502e
jtnoboru <kylelikewhoa@gmail.com>
2017-05-08 02:20:44 -0700

    Add files via upload

---
+++ confirmScreen.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import constants
+import user
+
+def Draw(state):
+    screen = np.zeros((800,800,3), np.uint8)
+    
+    cv2.putText(screen, "No", constants.UP_NAV, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+    cv2.putText(screen, "Are you sure?", (300, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+    cv2.putText(screen, "Yes", constants.DOWN_NAV, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+    cv2.imshow('BrushSmart', screen)
