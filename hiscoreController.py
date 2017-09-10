e97a9dab183e74a4cb3145d31768ab18cb74dd22
sbthegreat <connorbl93@yahoo.com>
2017-09-10 02:01:33 -0700

    trying to get this to work

--- hiscoreController.py
+++
@@ -1,9 +0,0 @@
-import hiscoreScreen
-import constants
-import user
-
-def Control(state):
-    hiscoreScreen.Draw(state)
-    if state.direction == constants.Direction.DOWN:
-        state.setPage(constants.Page.HOME)
-        state.resetDirection()
